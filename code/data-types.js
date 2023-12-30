c0caac81b499a567d5288c4b39c041a8f7cdf61f
JOSHIK <joshikroshan4021@gmail.com>
2023-12-30 19:49:36 +0000

    data-types added

--- code/data-types.js
+++ code/data-types.js
@@ -1,1 +1,20 @@
-console.log("hii");
+// we have 2 types on a high level
+// primitive and referential
+
+const num = 12
+const str = "nothing here"
+const temp = undefined
+const notAnNumber = NaN
+const boo = true //false also
+const bigint = 234567890123456789012345678901234567890
+const nu_ll = null
+const sym = Symbol("onvsdnv")
+
+console.log(typeof num)
+console.log(typeof str)
+console.log(typeof temp)
+console.log(typeof notAnNumber)
+console.log(typeof boo)
+console.log(typeof bigint)
+console.log(typeof nu_ll)  // this prints as object
+console.log(typeof sym)
