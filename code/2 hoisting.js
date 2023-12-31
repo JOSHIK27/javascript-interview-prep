ac31d4cabe88e956912c7009d331d07d5ddd5aa1
JOSHIK <joshikroshan4021@gmail.com>
2023-12-31 19:17:52 +0000

    hoisting updated

--- code/2 hoisting.js
+++ code/2 hoisting.js
@@ -1,19 +1,21 @@
-str = "hiii"
+// hositing : it is behaviour of javascript to bring the js variables/ functions to move to the top of their scope 
+// var are initialised with undefined after moving to the top
+// let and const are hoisted but not initialised
 
+something()
 
-var str;
-console.log(str)
-
-
-//variable declarations are hoisted not initialisation
-// to avoid hoisitng use-strict
-
-
-nothing()
-function nothing() {
-    console.log('does nothing')
+function something() {
+    console.log("calling the hosited function")
 }
 
-var x;
-console.log(x); // Outputs "undefined" since the initialization of "x" is not hoisted
-x = 23;
+console.log(a) // prints undefined . Variable a is hoisted by moving to the top of its scope and inialised with undeifned value
+
+var a = 10
+
+
+console.log(b) //2 hoisting.js:9 Uncaught ReferenceError: Cannot access 'b' before initialization
+// this says that, b is hoisted i.e moved top of its scope but not initialised same with const
+
+let b = 20
+
+
