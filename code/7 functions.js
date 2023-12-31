be954b705f91a70d62ecf8cd9d57d5e0da6d874e
JOSHIK <joshikroshan4021@gmail.com>
2023-12-31 16:08:22 +0000

    few more concepts added

---
+++ code/7 functions.js
@@ -0,0 +1,13 @@
+function solve() {
+    console.log("nothing")
+}
+
+const solve2 = function() {
+    console.log("nothing 1")
+}
+
+const solve3 = () => {
+    console.log("nothing 2")
+}
+
+console.log(solve(), solve2(), solve3())
---
+++ code/8 rest-spread.js
@@ -0,0 +1,11 @@
+// rest and spread 2 most commonly used ones
+
+// rest is used to convert any number of paramters to an array (...args)
+// most importantly rest parameter should be at the last of paramters list in the function
+
+function solve(...args) {
+    console.log(args[0], args[1], args[2])
+}
+
+solve(1, "2", null)
+
